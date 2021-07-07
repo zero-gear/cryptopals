10f43368b643308c1c2511d685669090b3d6cd40
Oleh Franchuk <0x00.gear@gmail.com>
2021-07-07 16:40:49 +0300

    [COMMON] define project structure

---
+++ src/hex2base64/mod.rs
@@ -0,0 +1,3 @@
+pub fn encode() {
+    println!("\t\t[*] hex2base64()")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{load_yaml, App};
+mod hex2base64;
+
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    #[warn(unused_variables)]
+    let matches = App::from(yaml).get_matches();
+
+    if let Some(challenge) = matches.value_of("challenge") {
+        match challenge {
+            "1.1" => {
+                println!("Challenge #{} 'Convert hex to base64'", challenge);
+                if let Some(input) = matches.value_of("INPUT") {
+                    println!("\tLet's handle next input: `{}`", input);
+                    hex2base64::encode();
+                }
+            }
+            _ => println!("Rest cases"),
+        }
+    }
 }
